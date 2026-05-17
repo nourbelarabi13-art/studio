6a78c81761d6b30d8ceb6b6e8be567ada353be49
Nour Belarabi <nour.belarabi13@gmail.com>
2026-05-17 18:38:29 +0000

    "In src/app/community/page.tsx, fix the React warning on line 293. Chang

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -290,7 +290,7 @@ export default function CommunityPage() {
                    >
                      <Camera className="w-6 h-6" />
                    </Button>
-                   <input type="file" fileInputRef={fileInputRef} className="hidden" accept="image/*" onChange={handleImageUpload} />
+                   <input type="file" ref={fileInputRef} className="hidden" accept="image/*" onChange={handleImageUpload} />
                 </div>
               </div>
               <Button 
