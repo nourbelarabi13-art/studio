a037c2b56bf3fb9fd8a0c7916171ab9893c083d6
Nour Belarabi <nour.belarabi13@gmail.com>
2026-05-17 18:11:12 +0000

    File changes

---
+++ src/app/robots.ts
@@ -0,0 +1,5 @@
+/** 
+ * DEPRECATED: This file has been neutralized to fix the Netlify build crash.
+ * The static SEO policy now resides strictly in /public/robots.txt.
+ */
+export default function robots() { return null; }
---
+++ src/app/sitemap.ts
@@ -0,0 +1,5 @@
+/** 
+ * DEPRECATED: This file has been neutralized to fix the Netlify build crash.
+ * The static sitemap now resides strictly in /public/sitemap.xml.
+ */
+export default function sitemap() { return []; }
